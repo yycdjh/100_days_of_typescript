3577ae52b8ddbd229ba051a54b55be468ba865aa
JD <591883039@qq.com>
2024-07-17 18:07:01 +0800

    feat: add day1 ui

--- src/utils/map-menus.ts
+++ src/utils/map-menus.ts
@@ -1,7 +1,7 @@
 export function mapMenusToRotes() {
   // 动态导入 views 目录下所有子目录中的 index.vue 文件
   const modules = import.meta.glob("../views/*/index.vue");
-  const routes = Object.keys(modules).map((path) => {
+  const routes = Object.keys(modules).map((path: any) => {
     const name = path.match(/\.\/views\/(.*)\/index\.vue$/)[1];
     return {
       path: `/${name}`,
--- src/views/Day1/index.vue
+++ src/views/Day1/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="h-full bg-green-100"></div>
+  <div class="h-full flex items-center justify-center bg-green-100">
+    <div class="flex w-3/4 bg-white">
+      <div class="flex w-full">
+        <div class="w-1/2">
+          <div class="mt-3 ml-3">Grid Width</div>
+          <div class="w-3/4 mt-3 ml-5 flex">
+            <el-slider class="mr-5" v-model="gridWidth" :min="0" :max="35" />
+            <span>{{ gridWidth }}</span>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="mt-3 ml-3">Grid Height</div>
+          <div class="w-3/4 mt-3 ml-5 flex">
+            <el-slider class="mr-5" v-model="gridHeight" :min="0" :max="35" />
+            <span>{{ gridHeight }}</span>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const gridWidth = ref(0);
+const gridHeight = ref(0);
+</script>
 
 <style scoped></style>
