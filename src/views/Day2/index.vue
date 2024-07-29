248465ae60c0133257a1ca9b7edf089ba447bd15
JD <591883039@qq.com>
2024-07-29 23:53:33 +0800

    feat: init day2

--- src/views/Day2/index.vue
+++ src/views/Day2/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>富文本编辑器</div>
+  <div
+    class="container h-full w-full flex flex-col items-center justify-center bg-green-100"
+  >
+    <div class="w-3/4 flex flex-col bg-white">
+      <div class="button-layout">
+        <el-button link>B</el-button>
+      </div>
+      <div class="text-input"></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
